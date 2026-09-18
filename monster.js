8e1bf1f2e87478ba09a4c7a6c0c8599ec6b8e355
NinezS2 <gabriellezukeram@gmail.com>
2026-09-18 14:07:45 -0400

    Add files via upload

---
+++ monster.js
@@ -0,0 +1,14 @@
+let booleanVar= false;
+
+function boo(){
+    if (booleanVar == false) {
+        document.getElementById().style.display = "block";
+        document.getElementById().stye.display = "none";
+        booleanVar = true;
+    }
+    else{
+        document.getElementById().style.display = "none";
+        document.getElementById().style.display = "block";
+        booleanVar = false;
+    }
+}
