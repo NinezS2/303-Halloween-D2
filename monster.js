c636237fbf34a0d3b7e5e7f2ba504861dabbbee7
NinezS2 <gabriellezukeram@gmail.com>
2026-09-18 14:18:57 -0400

    Add files via upload

--- monster.js
+++ monster.js
@@ -2,13 +2,13 @@ let booleanVar= false;
 
 function boo(){
     if (booleanVar === false) {
-        document.getElementById("gato").style.display = "block";
-        document.getElementById("bo").style.display = "none";
+        document.getElementById("bo").style.display = "block";
+        document.getElementById("gato").style.display = "none";
         booleanVar = true;
     }
     else{
-        document.getElementById("gato").style.display = "none";
-        document.getElementById("bo").style.display = "block";
+        document.getElementById("bo").style.display = "none";
+        document.getElementById("gato").style.display = "block";
         booleanVar = false;
     }
 }
